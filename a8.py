c7bc4b1045d587dbdc53b104616c7e16eee8d7b3
annaycc24 <athena1212624@gmail.com>
2020-11-10 03:09:23 -0600

    retrieved the airports' names

--- a8.py
+++ a8.py
@@ -3,6 +3,9 @@ import requests
 import lxml.html
 import re
 import pandas as pd
+import networkx as nx
+from geographiclib.geodesic import Geodesic
+geod = Geodesic.WGS84
 
 # use curl 99.74.38.205:30003 -o"livedata.csv" to load live data into file --> doesn't meet the requirement
 # f = pd.read_csv("livedata.csv")
@@ -25,13 +28,33 @@ def load_adsb_file(filename: str) -> pd.DataFrame:
 # load in the file
 airtraffic = 'airtraffic_20200710.csv'
 af = load_adsb_file(airtraffic)
+airports = pd.read_csv("airports.csv")
+countries = pd.read_csv("countries.csv")
+
 # print(af)
 # save unique flight number
 flight_num = (af.loc[af["Callsign/FlightNum"].notna()])["Callsign/FlightNum"].unique()
 # get request from website and load into json
 for i in range(len(flight_num)):
+    print("!") # 只是用來區分不同的fight_num跑出來的結果
     flight_num[i] = flight_num[i].strip()
     url = "https://opensky-network.org/api/routes?callsign=" + flight_num[i]
     r = requests.get(url)
     if r.status_code == requests.codes.ok:
-        print(r.json())
+        route = r.json()["route"]
+        print(route) # ['KORD', 'KSDF']
+        # 以下迴圈跑route中的點並取出需要的資訊
+        for j in range(len(route)):
+            airport_name = (airports.loc[airports["ident"] == route[j]])["name"]
+            # type(airport_name)是一個series
+            # 取出對應的機場名稱了，之後加入graph變成node
+            airport_name = airport_name.values[0]
+            # Chicago O'Hare International Airport
+            # Louisville Muhammad Ali International Airport
+            print(airport_name)
+            # G = nx.Graph()
+            # G.add_node("airport_name")
+            # 可能需要設last_position, current_position之類的變數來儲存位置，因為底下要算距離
+            # geographic和networkx已經import了，可以直接用(geod, nx)
+            # airpots.csv的B column是ICAO，找出對照的機場名稱(D column)，可能用(airports.loc[...])[name]來取？
+
