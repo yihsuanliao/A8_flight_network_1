08a281a5eb4a95b70f8c3d77df739beb9a4f842c
annaycc24 <athena1212624@gmail.com>
2020-11-11 02:25:04 -0600

    edited the retrieving data and adding nodes parts

--- a8.py
+++ a8.py
@@ -42,56 +42,73 @@ countries = pd.read_csv("countries.csv")
 # print(af)
 
 # save unique flight number
-flight_num = (af.loc[af["Callsign/FlightNum"].notna()])["Callsign/FlightNum"].unique()
-g = nx.DiGraph()
-# get request from website and load into json
-for i in range(len(flight_num)):
-    #print("!") # 只是用來區分不同的fight_num跑出來的結果
-    flight_num[i] = flight_num[i].strip()
-    url = "https://opensky-network.org/api/routes?callsign=" + flight_num[i]
-    r = requests.get(url)
-
-    if r.status_code == requests.codes.ok:
-        route = r.json()["route"]
-        #print(route) # ['KORD', 'KSDF']
-        # 以下迴圈跑route中的點並取出需要的資訊
-        last_lat = 0
-        last_long = 0
-        distance = 0
-        for j in range(len(route)):
-            airport_data = airports.loc[airports["ident"] == route[j]]
-            airport_name = airport_data["name"]
-            # type(airport_name)是一個series
-            # 取出對應的機場名稱了，之後加入graph變成node
-            airport_name = airport_name.values[0]
-            # Chicago O'Hare International Airport
-            # Louisville Muhammad Ali International Airport
-
-            airport_lat = airport_data["latitude_deg"].values[0]
-            airport_long = airport_data["longitude_deg"].values[0]
-            if j != 0:
-                distance = cal_distance(airport_lat, airport_long, last_lat, last_long)
-            last_lat = airport_lat
-            last_long = airport_long
-            #print(distance)
-            country_name = airport_data["iso_country"].values[0] # US
-            countries_data = (countries.loc[countries["code"] == country_name])["name"].values[0]
-            #print(airport_name)
-            #print("countries_data", countries_data)
-            g.add_node(route[j], name=airport_name, country=countries_data, latitude=airport_lat, longitude=airport_long)
-            if j != 0:
-                g.add_edge(route[j - 1], route[j], distance=distance)
-            #print("show nodes data", nx.get_node_attributes(g, 'longitude'))
-            #print("show edges:", g.edges.data())
-#print(flight_num)
-# plt.draw_networkx_nodes(g, pos[list(g.nodes)])
-
-
+# flight_num = (af.loc[af["Callsign/FlightNum"].notna()])["Callsign/FlightNum"].unique()
+callsign_lst = []
 # select distinct aircrafthex to callsign
-af1 = pd.DataFrame(af.groupby(['Callsign/FlightNum'])['AircraftHex'].unique())
 af = af.dropna(subset = ['Callsign/FlightNum'])
 af2 = pd.DataFrame(af.groupby(['AircraftHex'])['Callsign/FlightNum'].unique())
-print(af2)
+# print(af2)
+# aircraft_unique = list(af2.index)
+aircraft_num = af2.shape[0] # 1032
+callsign_col = af2["Callsign/FlightNum"]
+g = nx.DiGraph()
+# get request from website and load into json
+for i in range(aircraft_num):
+    #print("!") # 只是用來區分不同的fight_num跑出來的結果
+    callsign_num = len(callsign_col.values[i]) # 有可能一個航班有一到三個callsign
+    for j in range(callsign_num):
+        flight_num = af2["Callsign/FlightNum"].values[i][j].strip()
+        if flight_num not in callsign_lst:
+            callsign_lst.append(flight_num)
+            #print(callsign_lst)
+            url = "https://opensky-network.org/api/routes?callsign=" + flight_num
+            r = requests.get(url)
+            if r.status_code == requests.codes.ok:
+                route = r.json()["route"]
+                if len(route) > 2:
+                    print(route) # ['KORD', 'KSDF']
+                # 以下迴圈跑route中的點並取出需要的資訊
+                last_lat = 0
+                last_long = 0
+                distance = 0
+                for k in range(len(route)):
+                    airport_data = airports.loc[airports["ident"] == route[k]]
+                    airport_name = airport_data["name"]
+                    # type(airport_name)是一個series
+                    # 取出對應的機場名稱了，之後加入graph變成node
+                    airport_name = airport_name.values[0]
+                    # Chicago O'Hare International Airport
+                    # Louisville Muhammad Ali International Airport
+                    #print(airport_name)
+
+                    airport_lat = airport_data["latitude_deg"].values[0]
+                    airport_long = airport_data["longitude_deg"].values[0]
+                    if k != 0:
+                        distance = cal_distance(airport_lat, airport_long, last_lat, last_long)
+                    last_lat = airport_lat
+                    last_long = airport_long
+                    #print(distance)
+                    country_name = airport_data["iso_country"].values[0] # US
+                    countries_data = (countries.loc[countries["code"] == country_name])["name"].values[0]
+                    #print(airport_name)
+                    #print("countries_data", countries_data)
+                    g.add_node(route[k], name=airport_name, country=countries_data, latitude=airport_lat, longitude=airport_long)
+                    if k != 0:
+                        g.add_edge(route[k - 1], route[k], distance=distance)
+                    if len(route) > 2:
+                        #print("show nodes data", nx.get_node_attributes(g, 'longitude'))
+                        print("show nodes data", g.nodes)
+                        print("show edges:", g.edges.data())
+# TODO: a LIST of all FlightNumbers seen that fly that same route
+# 若FlightNumbers是指aircrafthex的話，再問問看是不是一個callsign只會有一個飛行路線，是的話跑callsign的list，然後看哪些aircrafthex有包含該callsign，就代表這些aircrafthex有相同的飛行路線
+# 若是指callsign的話，就要把route拿出來，然後看和哪些callsign的route相符合
+
+# select distinct aircrafthex to callsign
+#af1 = pd.DataFrame(af.groupby(['Callsign/FlightNum'])['AircraftHex'].unique())
+#af = af.dropna(subset = ['Callsign/FlightNum'])
+#af2 = pd.DataFrame(af.groupby(['AircraftHex'])['Callsign/FlightNum'].unique())
+#print("af2")
+#print(af2)
 
 
 # How many distinct airports have been discovered?
@@ -129,33 +146,27 @@ __all__ = ["reciprocity", "overall_reciprocity"]
 
 def reciprocity(G, nodes=None):
     r"""Compute the reciprocity in a directed graph.
-
     The reciprocity of a directed graph is defined as the ratio
     of the number of edges pointing in both directions to the total
     number of edges in the graph.
     Formally, $r = |{(u,v) \in G|(v,u) \in G}| / |{(u,v) \in G}|$.
-
     The reciprocity of a single node u is defined similarly,
     it is the ratio of the number of edges in both directions to
     the total number of edges attached to node u.
-
     Parameters
     ----------
     G : graph
        A networkx directed graph
     nodes : container of nodes, optional (default=whole graph)
        Compute reciprocity for nodes in this container.
-
     Returns
     -------
     out : dictionary
        Reciprocity keyed by node label.
-
     Notes
     -----
     The reciprocity is not defined for isolated nodes.
     In such cases this function will return None.
-
     """
     # If `nodes` is not specified, calculate the reciprocity of the graph.
     if nodes is None:
@@ -198,14 +209,11 @@ def _reciprocity_iter(G, nodes):
 
 def overall_reciprocity(G):
     """Compute the reciprocity for the whole graph.
-
     See the doc of reciprocity for the definition.
-
     Parameters
     ----------
     G : graph
        A networkx graph
-
     """
     n_all_edge = G.number_of_edges()
     n_overlap_edge = (n_all_edge - G.to_undirected().number_of_edges()) * 2
